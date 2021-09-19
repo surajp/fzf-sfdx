9647c4f34627099a48a6a9c5ead873ef29068a49
Suraj Pillai <85.suraj@gmail.com>
2021-09-19 17:40:51 -0400

    Make bash script self contained, like zsh
    To make it easier for users to install, added all fzf commands to the
    bash script so they can simply replace the default fzf keybindings file
    with this file.

--- fzf-key-bindings.bash
+++ fzf-key-bindings.bash
@@ -1,3 +1,63 @@
+#     ____      ____
+#    / __/___  / __/
+#   / /_/_  / / /_
+#  / __/ / /_/ __/
+# /_/   /___/_/ key-bindings.bash
+#
+# - $FZF_TMUX_OPTS
+# - $FZF_CTRL_T_COMMAND
+# - $FZF_CTRL_T_OPTS
+# - $FZF_CTRL_R_OPTS
+# - $FZF_ALT_C_COMMAND
+# - $FZF_ALT_C_OPTS
+
+# Key bindings
+# ------------
+__fzf_select__() {
+  local cmd="${FZF_CTRL_T_COMMAND:-"command find -L . -mindepth 1 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
+    -o -type f -print \
+    -o -type d -print \
+    -o -type l -print 2> /dev/null | cut -b3-"}"
+  eval "$cmd" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse --bind=ctrl-z:ignore $FZF_DEFAULT_OPTS $FZF_CTRL_T_OPTS" $(__fzfcmd) -m "$@" | while read -r item; do
+    printf '%q ' "$item"
+  done
+  echo
+}
+
+if [[ $- =~ i ]]; then
+
+__fzfcmd() {
+  [ -n "$TMUX_PANE" ] && { [ "${FZF_TMUX:-0}" != 0 ] || [ -n "$FZF_TMUX_OPTS" ]; } &&
+    echo "fzf-tmux ${FZF_TMUX_OPTS:--d${FZF_TMUX_HEIGHT:-40%}} -- " || echo "fzf"
+}
+
+fzf-file-widget() {
+  local selected="$(__fzf_select__)"
+  READLINE_LINE="${READLINE_LINE:0:$READLINE_POINT}$selected${READLINE_LINE:$READLINE_POINT}"
+  READLINE_POINT=$(( READLINE_POINT + ${#selected} ))
+}
+
+__fzf_cd__() {
+  local cmd dir
+  cmd="${FZF_ALT_C_COMMAND:-"command find -L . -mindepth 1 \\( -path '*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' -o -fstype 'proc' \\) -prune \
+    -o -type d -print 2> /dev/null | cut -b3-"}"
+  dir=$(eval "$cmd" | FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} --reverse --bind=ctrl-z:ignore $FZF_DEFAULT_OPTS $FZF_ALT_C_OPTS" $(__fzfcmd) +m) && printf 'cd %q' "$dir"
+}
+
+__fzf_history__() {
+  local output
+  output=$(
+    builtin fc -lnr -2147483648 |
+      last_hist=$(HISTTIMEFORMAT='' builtin history 1) perl -n -l0 -e 'BEGIN { getc; $/ = "\n\t"; $HISTCMD = $ENV{last_hist} + 1 } s/^[ *]//; print $HISTCMD - $. . "\t$_" if !$seen{$_}++' |
+      FZF_DEFAULT_OPTS="--height ${FZF_TMUX_HEIGHT:-40%} $FZF_DEFAULT_OPTS -n2..,.. --tiebreak=index --bind=ctrl-r:toggle-sort,ctrl-z:ignore $FZF_CTRL_R_OPTS +m --read0" $(__fzfcmd) --query "$READLINE_LINE"
+  ) || return
+  READLINE_LINE=${output#*$'\t'}
+  if [ -z "$READLINE_POINT" ]; then
+    echo "$READLINE_LINE"
+  else
+    READLINE_POINT=0x7fffffff
+  fi
+}
 
 __fzf_sfdx_flags__(){
   local selected="$1"
@@ -9,7 +69,7 @@ __fzf_sfdx_flags__(){
   echo "${ret//$'\n'/ --}"
 }
 
-__fzf_sfdx__(){
+fzf-sfdx(){
   local fullcmd="$READLINE_LINE"
   local cmd="$(echo $fullcmd | awk '{print $1}')"
   local subcmd="$(echo $fullcmd | awk '{print $2}')"
@@ -19,10 +79,12 @@ __fzf_sfdx__(){
     local flag="$(__fzf_sfdx_flags__ $subcmd $fullcmd)"
     if [[ "$flag" != "" ]]
     then
-      READLINE_LINE="$fullcmd --$flag"
-      READLINE_POINT=$(( ${#fullcmd} + ${#flag} + 3 ))
+      READLINE_LINE="${READLINE_LINE:0:$READLINE_POINT}--$flag${READLINE_LINE:$READLINE_POINT}"
+      READLINE_POINT=$(( READLINE_POINT + ${#flag} + 3 ))
+      #READLINE_LINE="$fullcmd --$flag"
+      #READLINE_POINT=$(( ${#fullcmd} + ${#flag} + 3 ))
     fi
-  elif [[ "$mcd" == "sfdx" || "$cmd" == "" ]]
+  elif [[ "$cmd" == "sfdx" || "$cmd" == "" ]]
   then
     local querystr=""
     if [[ "$subcmd" != "" ]]; then
@@ -36,18 +98,44 @@ __fzf_sfdx__(){
   fi
 }
 
+# Required to refresh the prompt after fzf
+bind -m emacs-standard '"\er": redraw-current-line'
 
-if [ "${BASH_VERSINFO[0]}" -lt 4 ]; then
+bind -m vi-command '"\C-z": emacs-editing-mode'
+bind -m vi-insert '"\C-z": emacs-editing-mode'
+bind -m emacs-standard '"\C-z": vi-editing-mode'
 
-  # CTRL-K - Search through sfdx commands
-  bind -m vi-command '"\C-e": "\C-z\C-e\C-z"'
-  bind -m vi-insert '"\C-e": "\C-z\C-e\C-z"'
+if (( BASH_VERSINFO[0] < 4 )); then
+  # CTRL-T - Paste the selected file path into the command line
+  bind -m emacs-standard '"\C-t": " \C-b\C-k \C-u`__fzf_select__`\e\C-e\er\C-a\C-y\C-h\C-e\e \C-y\ey\C-x\C-x\C-f"'
+  bind -m vi-command '"\C-t": "\C-z\C-t\C-z"'
+  bind -m vi-insert '"\C-t": "\C-z\C-t\C-z"'
 
+  # CTRL-R - Paste the selected command from history into the command line
+  bind -m emacs-standard '"\C-r": "\C-e \C-u\C-y\ey\C-u"$(__fzf_history__)"\e\C-e\er"'
+  bind -m vi-command '"\C-r": "\C-z\C-r\C-z"'
+  bind -m vi-insert '"\C-r": "\C-z\C-r\C-z"'
 else
+  # CTRL-T - Paste the selected file path into the command line
+  bind -m emacs-standard -x '"\C-f": fzf-file-widget'
+  bind -m vi-command -x '"\C-f": fzf-file-widget'
+  bind -m vi-insert -x '"\C-f": fzf-file-widget'
 
-  # CTRL-K - Search through sfdx commands
-  bind -m emacs-standard -x '"\C-e": __fzf_sfdx__'
-  bind -m vi-command -x '"\C-e": __fzf_sfdx__'
-  bind -m vi-insert -x '"\C-e": __fzf_sfdx__'
+  # CTRL-R - Paste the selected command from history into the command line
+  bind -m emacs-standard -x '"\C-r": __fzf_history__'
+  bind -m vi-command -x '"\C-r": __fzf_history__'
+  bind -m vi-insert -x '"\C-r": __fzf_history__'
+
+  # CTRL-e - Search for and paste the selected sfdx command onto the command line
+  bind -m emacs-standard -x '"\C-e": fzf-sfdx'
+  bind -m vi-command -x '"\C-e": fzf-sfdx'
+  bind -m vi-insert -x '"\C-e": fzf-sfdx'
+
+fi
+
+# ALT-C - cd into the selected directory
+bind -m emacs-standard '"\ec": " \C-b\C-k \C-u`__fzf_cd__`\e\C-e\er\C-m\C-y\C-h\e \C-y\ey\C-x\C-x\C-d"'
+bind -m vi-command '"\ec": "\C-z\ec\C-z"'
+bind -m vi-insert '"\ec": "\C-z\ec\C-z"'
 
 fi
